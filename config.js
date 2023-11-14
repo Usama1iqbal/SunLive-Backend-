0f741fba65c2bb8a09473c7cbe12961dc5806918
“Usama1iqbal” <usamaiqbal117@gmail.com>
2023-11-14 22:16:35 +0500

    all commit

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // server database
+  MONGODB_USERNAME: "ourliveapps", // Your db username
+  MONGODB_PASSWORD: "Jahangir500", // Your db password
+  MONGODB_DB_NAME: "Bigee-Live", // Your db name
+
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "JVHZoASnmYH4FNWt1LACllboPHOblUSK", // your app secret key
+
+  //gmail credentials for send email
+  EMAIL: "ourliveapps@gmail.com", // your email address
+  PASSWORD: "Jahangir@500", // your email password
+
+  //secret key for jwt
+  JWT_SECRET: "FdsvDYdvnX4rddvd9I",
+
+  SERVER_PATH: "http://localhost:5000/", // your app base URL
+
+  // firebase server key for notification
+  SERVER_KEY:
+    "AAAAU3VJZFg:APA91bGnBGt6GOxoVNkCebUqVTzakV2DSJ6NSudCjnQbd2j8xh76sg5QPvIZ46YYeKHhXzt6GZ9BRvRwv_2AissqiGdMAmsVDybzke219LYBCy_laM4yzlanbJBMR46skofXqMTOK2HK", // your firebase server key
+};
+
+// 7u2O0tkWq1RP9gEy2I
+
+// mongodb+srv://ourliveapps:Jahangir500@cluster0.ggrrv9s.mongodb.net/test
